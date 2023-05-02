b93ce0affb4f6c9833778325f10eb1bd004e2d2d
IbraHam733 <98653954+ibrahim2375@users.noreply.github.com>
2023-05-02 03:50:18 +0300

    test seed

---
+++ src/seed/food.seed.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import AsyncHandler from 'express-async-handler';
+import { FoodModel } from '../models/food.model';
+import { sample_foods } from '../data';
+
+const router = Router();
+//seed data to cluster 
+router.get('/foods', AsyncHandler(
+    async (req, res) => {
+        const foodCount = await FoodModel.countDocuments();
+        if(foodCount > 0) {
+            res.send('food seed already done!');
+            return;
+        }
+        await FoodModel.create(sample_foods);
+        res.send('Food Seed Done!');
+    }
+))
+
+export default router;
